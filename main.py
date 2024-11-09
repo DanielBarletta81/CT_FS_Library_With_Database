d9762032d531ff2d0adc4470760ca327d86abe10
DanielBarletta81 <dbarcelona1481@gmail.com>
2024-11-09 18:18:22 -0500

    initial upload

---
+++ main.py
@@ -0,0 +1,76 @@
+# main
+
+#import mysql.connector
+
+#from mysql.connector import Error
+
+import books, genres, authors, users
+
+
+db_name = "libraryDb"
+user = "root"
+password = "Babinz2023!"
+host = "localhost"
+
+
+
+def main():
+  while True:
+     """   # establish connection
+    conn = mysql.connector.connect(buffered=True,
+            database = db_name,
+            user = user,
+            password = password,
+            host = host
+            )
+    if conn is not None: """
+     
+     print("***  Welcome to the Library Management System! ***")
+     print("\n Menu:")
+     print("\n 1. Book Operations Menu.")
+     print("\n 2. Author Operations Menu.")
+     print("\n 3. Genre Operations Menu. ")
+     print("\n 4. User Operations Menu. ")
+     print("\n 5. Quit. ")
+
+     choice = int(input("Please choose an option (1-5): "))
+
+     if choice == 5:
+          return
+
+     try:
+
+            if choice == 1:
+               books.book_ops_menu()
+            
+              
+            elif choice == 2:
+
+               authors.author_menu()
+
+                   
+            elif choice == 3:
+                genres.genre_ops_menu()
+                
+
+            elif choice == 4:
+               users.user_menu()
+                
+            
+            else:
+                print("Input Error. Please select a valid choice.")
+
+
+     
+         
+     
+     except Exception as e:
+            print(f'An exception occurred {e}')
+    
+
+if __name__ == "__main__":
+     main()
+#Implement the following actions in response to menu selections 
+# using the classes you've created:
+#- Adding a new book with all relevant details.
+# - Allowing users to borrow a book, marking it as 
---
+++ notes.py
@@ -0,0 +1,72 @@
+
+# 
+#  create a robust system that allows users to browse, borrow, return, 
+# and explore a collection of books and the use of modules.
+
+
+
+#Apply encapsulation principles by defining private attributes and using getters and setters
+#  for necessary data access.
+
+
+#Inheritance and Polymorphism:
+
+#Utilize inheritance to give your books a Genre. Your book class can inherit from the 
+# Genre class to gain attributes like genre name, description, and category. 
+# Or create book subclasses like FictionBook, NonFictionBook that inherit from the Book class. 
+
+
+#Modules:
+
+#Organize your code into modules to promote code organization and maintainability. 
+# Create separate modules for classes, user interactions, and error handling.
+
+""" 
+Menu Actions:
+
+- Adding a new book with all relevant details.
+- Allowing users to borrow a book, marking it as "Borrowed."
+- Allowing users to return a book, marking it as "Available."
+- Searching for a book by its unique identifier (ISBN or title) and displaying its details.
+- Displaying a list of all books with their unique identifiers.
+
+
+- Adding a new user with user details.
+- Viewing user details.
+- Displaying a list of all users.
+
+- Adding a new author with author details.
+- Viewing author details.
+- Displaying a list of all authors.
+
+- Adding a new genre with genre details.
+- Viewing genre details.
+- Displaying a list of all genres.
+ """
+
+
+#User Interaction:
+
+#Utilize the input() function within the appropriate menus to enable users to interact
+#  with the CLI and select menu options.
+#Implement input validation using regular expressions (regex) to ensure the correct 
+# formatting of user input.
+
+
+#Error Handling:
+
+#Implement error handling using try, except, else, and finally 
+# blocks to manage potential issues gracefully, such as incorrect user input or file operations.
+
+
+#GitHub Repository:
+
+#Create a GitHub repository for your project and commit code regularly.
+#Maintain a clean and interactive README.md file in your GitHub repository, providing clear instructions on how to run the application and explanations of its features.
+#Include a link to your GitHub repository in your project documentation.
+
+
+#Project Tips
+#Begin by designing a class hierarchy that represents the library's structure and entities.
+#Test your code iteratively as you implement each feature to identify and address any potential bugs or issues.
+#Collaborate with peers, seek assistance, and remember that learning is a collaborative effort.
